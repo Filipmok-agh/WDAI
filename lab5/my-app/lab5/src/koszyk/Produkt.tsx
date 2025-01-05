c738053dd99b80efed2695644e7978626c43e319
Filip <filipmok@student.agh.edu.pl>
2025-01-05 19:26:10 +0100

    reupload

--- lab5/my-app/lab5/src/koszyk/Produkt.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-function Produkt(props: { nazwa: string }) {
-  return <p>Nazwa produktu: {props.nazwa}</p>
-}
-export default Produkt;
--- lab5/my-app/lab5/src/licznik/przycisk.tsx
+++
@@ -1,8 +0,0 @@
-
-function Przycisk(props: { onClick: () => void }) {
-  return (
-    <button onClick={props.onClick}>Dodaj</button>
-  );
-}
-
-export default Przycisk;
